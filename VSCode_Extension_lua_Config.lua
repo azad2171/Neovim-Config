c72c76cba86cce2c30214c16679eda2cde3c95a6
azad2171 <84909395+azad2171@users.noreply.github.com>
2025-05-24 18:20:24 +0530

    Added config

--- VSCode_Extension_lua_Config.lua
+++ VSCode_Extension_lua_Config.lua
@@ -42,3 +42,58 @@ keymap({"n", "v"}, "<leader>sp", "<cmd>lua require('vscode').action('workbench.a
 -- navigate between tabs
 keymap("n", "<leader>h", "<cmd>lua require('vscode').action('workbench.action.previousEditor')<CR>", opts)
 keymap("n", "<leader>l", "<cmd>lua require('vscode').action('workbench.action.nextEditor')<CR>", opts)
+
+
+
+
+-- fold unfold code
+keymap("n", "<leader>f", "<cmd>lua require('vscode').action('editor.fold')<CR>", opts)
+keymap("n", "<leader>u", "<cmd>lua require('vscode').action('editor.unfold')<CR>", opts)
+
+
+
+
+-- move text up and down
+keymap("n", "K", ":m .-2<CR>==", opts)
+keymap("n", "J", ":m .+1<CR>==", opts)
+
+keymap("v", "K", ":m '<-2<CR>gv==gv", opts)
+keymap("v", "J", ":m '>+1<CR>gv==gv", opts)
+
+keymap({"n", "v"}, "L", "$", opts)
+keymap({"n", "v"}, "H", "^", opts)
+
+
+-- change sidebar width
+keymap("n", "<leader>-", "<cmd>lua require('vscode').action('workbench.action.increaseViewSize')<CR>", opts)
+keymap("n", "<leader>+", "<cmd>lua require('vscode' ).action('workbench.action.decreaseViewSize')<CR>", opts)
+
+-- Map [c to navigate to the previous Git diff
+keymap("n", "[c", "<cmd>lua require('vscode').action('workbench.action.compareEditor.previousChange')<CR>", opts)
+
+-- Map ]c to navigate to the next Git diff
+keymap("n", "]c", "<cmd>lua require('vscode').action('workbench.action.compareEditor.nextChange' )<CR>", opts)
+
+-- Split left
+keymap("n", "<C-h>", "<cmd>lua require('vscode').action('workbench.action.splitEditorLeft')<CR>", opts)
+-- Split down
+keymap("n", "<C-j>", "<cmd>lua require('vscode').action('workbench.action.splitEditorDown' )<CR>", opts)
+-- Split up
+keymap("n", "<C-k>", "<cmd>lua require('vscode').action('workbench.action.splitEditorUp')<CR>", opts)
+-- Split right
+keymap("n", "<C-1>", "<cmd>lua require('vscode' ).action('workbench.action.splitEditorRight')<CR>", opts)
+
+
+-- Focus left editor group
+-- keymap("n", "<leader>h", "<cmd>lua require('vscode' ).action('workbench.action.focusLeftGroup' )<CR>",
+
+-- Focus down editor group
+keymap("n", "<leader>j", "<cmd>lua require('vscode'). action('workbench.action.focusBelowGroup' )<CR>", opts)
+
+-- Focus up editor group
+keymap("n", "<leader>k", "<cmd>lua require('vscode').action('workbench.action.focusAboveGroup' )<CR>", opts)
+
+-- Focus right editor group
+-- keymap("n", "<leader>1", "<cmd>lua require('vscode' ).action('workbench.action. focusRightGroup')<CR>",opts)
+
+
